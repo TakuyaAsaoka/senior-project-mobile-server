2a8435678bcff87ac07ad37219c927c50bd19509
Asaoka Takuya <dimas080201@gmail.com>
2023-06-28 02:53:31 +0900

    add:express-generatorとTSのセットアップ完了

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from 'express';
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req: Request, res: Response, next: (err: any) => void) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err: any, req: Request, res: Response, next: (err: any) => void) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
