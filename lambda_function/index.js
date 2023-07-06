84fc3f32b9fd07694d4015ceed36d8bebce9124c
yoshinori-matsubara <yoshinori_matsubara@mail.toyota.co.jp>
2023-07-06 17:21:44 +0900

    update:GET favoriteのレスポンスにhasVisitedNumber追加

--- lambda_function/index.js
+++ lambda_function/index.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const knex = require("./knex.js");
+const data = require("./data.js");
 
 // Lambda関数のエントリーポイント
 exports.handler = async (event, context) => {
@@ -72,33 +73,43 @@ exports.handler = async (event, context) => {
   }
 };
 
-// GET /api/cards/:user
+// GET /api/cards/:user/?prefecture
 async function getRandomCards(event, context) {
   try {
-    const blackList = [
-      "小杉農園",
-      "ポレポレ農園",
-      "佐倉きのこ園",
-      "無添加",
-      "ブルーベリー",
-      "直売所",
-      "海の公園",
-      "ふくろうの家",
-      "占い",
-    ];
-    const arrOfReq = [
-      "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303001&image=true",
-      "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303002&image=true",
-      "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303003&image=true",
-      "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303004&image=true",
-      "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303005&image=true",
-      "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303011&image=true",
-    ];
+    // const blackList = [
+    //   "小杉農園",
+    //   "ポレポレ農園",
+    //   "佐倉きのこ園",
+    //   "無添加",
+    //   "ブルーベリー",
+    //   "直売所",
+    //   "海の公園",
+    //   "ふくろうの家",
+    //   "占い",
+    // ];
+    // const arrOfReq = [
+    //   "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303001&image=true",
+    //   "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303002&image=true",
+    //   "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303003&image=true",
+    //   "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303004&image=true",
+    //   "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303005&image=true",
+    //   "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPU9USnZwMHRxWDQ5TCZzPWNvbnN1bWVyc2VjcmV0Jng9NDQ-&output=json&gc=0303011&image=true",
+    // ];
+
+    let prefecture = "";
+    let deck = data;
+    if (event.queryStringParameters) {
+      if (event.queryStringParameters.prefecture) {
+        prefecture = event.queryStringParameters.prefecture;
+        deck = data.filter((card) => card.prefecture === prefecture);
+      }
+    }
 
     function generateRandomArray(number, max) {
+      const actualNum = number <= deck.length ? number : deck.length;
       const result = [];
       // 重複のないランダムな数値を生成
-      while (result.length < number) {
+      while (result.length < actualNum) {
         const randomNumber = Math.floor(Math.random() * max);
         if (result.indexOf(randomNumber) === -1) {
           result.push(randomNumber);
@@ -107,24 +118,25 @@ async function getRandomCards(event, context) {
       return result;
     }
 
-    const arrOfRes = await Promise.all(
-      arrOfReq.map(async (url) => {
-        const res = await fetch(url);
-        const data = await res.json();
-        return data.Feature;
-      })
-    );
-    const rowCards = arrOfRes.flat();
-    const rowDeck = rowCards.filter((card) => card.hasOwnProperty("Geometry"));
-    const deck = rowDeck.filter((card) => {
-      let flag = true;
-      for (const elm of blackList) {
-        if (card.Name.includes(elm)) {
-          flag = false;
-        }
-      }
-      return flag;
-    });
+    // const arrOfRes = await Promise.all(
+    //   arrOfReq.map(async (url) => {
+    //     const res = await fetch(url);
+    //     const data = await res.json();
+    //     return data.Feature;
+    //   })
+    // );
+    // const rowCards = arrOfRes.flat();
+    // const rowDeck = rowCards.filter((card) => card.hasOwnProperty("Geometry"));
+    // const deck = rowDeck.filter((card) => {
+    //   let flag = true;
+    //   for (const elm of blackList) {
+    //     if (card.Name.includes(elm)) {
+    //       flag = false;
+    //     }
+    //   }
+    //   return flag;
+    // });
+
     const randNums = generateRandomArray(10, deck.length);
     const cards = deck.filter((card, index) => {
       let flag = false;
@@ -135,7 +147,6 @@ async function getRandomCards(event, context) {
       }
       return flag;
     });
-    console.log("枚数：", deck.length);
     const user = decodeURIComponent(event.pathParameters.user);
 
     // ユーザーのFAVORITEテーブルを取得
@@ -143,62 +154,61 @@ async function getRandomCards(event, context) {
 
     // レスポンスを作成
     const result = await Promise.all(
-      cards
-        .filter((card) => {
-          let flag = true;
-          for (const favorite of favorites) {
-            if (card.Name === favorite.name) {
-              flag = false;
-            }
+      cards.filter((card) => {
+        let flag = true;
+        for (const favorite of favorites) {
+          if (card.name === favorite.name) {
+            flag = false;
           }
-          return flag;
-        })
-        .map(async (card) => {
-          const point = card.Geometry.Coordinates.split(",");
-          const x = point[0];
-          const y = point[1];
-          const geoRes = await fetch(
-            `http://geoapi.heartrails.com/api/json?method=searchByGeoLocation&x=${x}&y=${y}`
-          ).then((data) => {
-            return data;
-          });
-          const geoData = await geoRes.json().then((data) => {
-            return data;
-          });
-          const prefecture = geoData.response.location[0].prefecture;
-          const postal = geoData.response.location[0].postal;
-          const zipCode = postal.slice(0, 3) + "-" + postal.slice(-4);
-          return {
-            name: card.Name ?? "",
-            prefecture: prefecture,
-            images: [card.Property.LeadImage],
-            price: card.Property.Price ? Number(card.Property.Price) : 0,
-            access: "電車",
-            zip_code: zipCode ?? "",
-            address: card.Property.Address ?? "",
-            business: "",
-            phone_number: card.Property.Tel1 ?? "",
-            parking:
-              card.Property.ParkingFlag === "1" ||
-              card.Property.ParkingFlag === "true"
-                ? "有り"
-                : card.Property.ParkingFlag === "0" ||
-                  card.Property.ParkingFlag === "false"
-                ? "無し"
-                : "",
-            toilet: "有り",
-            closed: "年中無休",
-            public_transport: card.Property.Station
-              ? card.Property.Station.map(
-                  (elm) => elm.Railway + " " + elm.Name + "駅"
-                )
-              : [],
-            car: [],
-            has_visited: false,
-            latitude: y,
-            longitude: x,
-          };
-        })
+        }
+        return flag;
+      })
+      // .map(async (card) => {
+      // const point = card.Geometry.Coordinates.split(",");
+      // const x = point[0];
+      // const y = point[1];
+      // const geoRes = await fetch(
+      //   `http://geoapi.heartrails.com/api/json?method=searchByGeoLocation&x=${x}&y=${y}`
+      // ).then((data) => {
+      //   return data;
+      // });
+      // const geoData = await geoRes.json().then((data) => {
+      //   return data;
+      // });
+      // const prefecture = geoData.response.location[0].prefecture;
+      // const postal = geoData.response.location[0].postal;
+      // const zipCode = postal.slice(0, 3) + "-" + postal.slice(-4);
+      // return {
+      //   name: card.Name ?? "",
+      //   prefecture: prefecture,
+      //   images: [card.Property.LeadImage],
+      //   price: card.Property.Price ? Number(card.Property.Price) : 0,
+      //   access: "電車",
+      //   zip_code: zipCode ?? "",
+      //   address: card.Property.Address ?? "",
+      //   business: "",
+      //   phone_number: card.Property.Tel1 ?? "",
+      //   parking:
+      //     card.Property.ParkingFlag === "1" ||
+      //     card.Property.ParkingFlag === "true"
+      //       ? "有り"
+      //       : card.Property.ParkingFlag === "0" ||
+      //         card.Property.ParkingFlag === "false"
+      //       ? "無し"
+      //       : "",
+      //   toilet: "有り",
+      //   closed: "年中無休",
+      //   public_transport: card.Property.Station
+      //     ? card.Property.Station.map(
+      //         (elm) => elm.Railway + " " + elm.Name + "駅"
+      //       )
+      //     : [],
+      //   car: [],
+      //   has_visited: false,
+      //   latitude: y,
+      //   longitude: x,
+      // };
+      // })
     );
 
     // プロキシ統合形式に準拠したレスポンスを構築
@@ -246,6 +256,9 @@ async function getFavorites(event, context) {
         result.forEach((element) => {
           if (element.name === favorite.prefecture) {
             element.number++;
+            if (favorite.has_visited) {
+              element.hasVisitedNumber++;
+            }
           }
         });
       } else {
@@ -253,10 +266,13 @@ async function getFavorites(event, context) {
           name: favorite.prefecture,
           imgSrc: favorite.images[0],
           number: 1,
+          hasVisitedNumber: favorite.has_visited ? 1 : 0,
         });
       }
     });
-
+    result.sort((a, b) => {
+      return b.number - a.number;
+    });
     // プロキシ統合形式に準拠したレスポンスを構築
     const response = {
       statusCode: 200,
